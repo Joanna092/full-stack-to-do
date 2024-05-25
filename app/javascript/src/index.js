ac2bea767dbe6f62040e16839403997071178dd4
Joanna092 <kochanowska92@gmail.com>
2024-05-25 12:43:23 +0900

    created a create task function

--- app/javascript/src/index.js
+++ app/javascript/src/index.js
@@ -1,65 +1,6 @@
-import "./requests.js";
 import $ from 'jquery';
+import { getAndDisplayAllTasks, createTask, deleteTask, markTaskComplete, markTaskActive } from './requests.js';
 
-import {
-    indexTasks,
-    postTask,
-    getAndDisplayAllTasks
-  } from "./requests.js";
-
-  indexTasks(function (response) {
-    var htmlString = response.tasks.map(function(task) {
-      return "<div class='col-12 mb-3 p-2 border rounded task' data-id='" + task.id + "'> \
-        " + task.content + "\
-        </div>";
-    });
-    $("#tasks").html(htmlString);
-  });
-
-
-  getAndDisplayAllTasks (function(response, textStatus) {
-    $('#todo-list').empty();
-
-
-    var filteredTasks = response.tasks.filter(function(task) {
-        return true;
-    })
-    filteredTasks.forEach(function(task) {
-        $('#todo-list').prepend('<div class="row d-flex"><div class="p-2"><input type="checkbox" class="mark-complete" data-id="' + task.id + '"' + (task.completed ? 'checked' : '') + '></div><div class="p-2"><p class="col-xs-8">' + task.content + '</p></div><div class="ml-auto p-2"><button class="delete" data-id="' + task.id + '"><i class="fa fa-trash" aria-hidden="true"></i></button></div></div><hr>');
-    });
-
-
-    $("#all").click(function(event) {
-        event.preventDefault();
-        $('#todo-list').empty();
-        var filteredTasks = response.tasks.filter(function(task) {
-            return true;
-        })
-        filteredTasks.forEach(function(task) {
-            $('#todo-list').prepend('<div class="row d-flex"><div class="p-2"><input type="checkbox" class="mark-complete" data-id="' + task.id + '"' + (task.completed ? 'checked' : '') + '></div><div class="p-2"><p class="col-xs-8">' + task.content + '</p></div><div class="ml-auto p-2"><button class="delete" data-id="' + task.id + '"><i class="fa fa-trash" aria-hidden="true"></i></button></div></div><hr>');
-        });
-    });
-
-    $("#completed").click(function(event) {
-        event.preventDefault();
-
-        $('#todo-list').empty();
-        var filteredTasks = response.tasks.filter(function(task) {
-            return task.completed === true;
-        })
-        filteredTasks.forEach(function(task) {
-            $('#todo-list').prepend('<div class="row d-flex"><div class="p-2"><input type="checkbox" class="mark-complete" data-id="' + task.id + '"' + (task.completed ? 'checked' : '') + '></div><div class="p-2"><p class="col-xs-8">' + task.content + '</p></div><div class="ml-auto p-2"><button class="delete" data-id="' + task.id + '"><i class="fa fa-trash" aria-hidden="true"></i></button></div></div><hr>');
-        });
-    });
-
-    $("#active").click(function(event) {
-        event.preventDefault();
-        $('#todo-list').empty();
-        var filteredTasks = response.tasks.filter(function(task) {
-            return task.completed === false;
-        })
-        filteredTasks.forEach(function(task) {
-            $('#todo-list').prepend('<div class="row d-flex"><div class="p-2"><input type="checkbox" class="mark-complete" data-id="' + task.id + '"' + (task.completed ? 'checked' : '') + '></div><div class="p-2"><p class="col-xs-8">' + task.content + '</p></div><div class="ml-auto p-2"><button class="delete" data-id="' + task.id + '"><i class="fa fa-trash" aria-hidden="true"></i></button></div></div><hr>');
-        });
-    });
-  });
+$(document).ready(function() {
+  getAndDisplayAllTasks();
+});
--- app/javascript/src/requests.js
+++ app/javascript/src/requests.js
@@ -1,9 +1,6 @@
-console.log('ajax requests');
 import $ from 'jquery';
 
-
-//tutorial follow up 
-
+console.log('ajax requests');
 
 $.ajaxSetup({
   headers: {
@@ -11,194 +8,116 @@ $.ajaxSetup({
   }
 });
 
-export var indexTasks = function (successCB, errorCB) {
-  var request = {
+// Function to get and display all tasks
+export var getAndDisplayAllTasks = function() {
+  $.ajax({
     type: 'GET',
     url: 'api/tasks?api_key=1',
-    success: successCB,
-    error: errorCB
-  }
-  $.ajax(request);
+    dataType: 'json',
+    success: function(response, textStatus) {
+      $('#todo-list').empty();
+      var filteredTasks = response.tasks.filter(function(task) {
+        return true;
+      });
+      filteredTasks.forEach(function(task) {
+        $('#todo-list').prepend('<div class="row d-flex"><div class="p-2"><input type="checkbox" class="mark-complete" data-id="' + task.id + '"' + (task.completed ? 'checked' : '') + '></div><div class="p-2"><p class="col-xs-8">' + task.content + '</p></div><div class="ml-auto p-2"><button class="delete" data-id="' + task.id + '"><i class="fa fa-trash" aria-hidden="true"></i></button></div></div><hr>');
+      });
+    },
+    error: function(request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+  });
 };
-indexTasks();
 
-
-export var postTask = function (content, successCB, errorCB) {
-    var request = {
-      type: 'POST',
-      url: 'api/tasks?api_key=1',
-      data: {
-        task: {
-          content: content
-        }
-      },
-      success: successCB,
-      error: errorCB
+// Function to create a new task
+export var createTask = function() {
+  $.ajax({
+    type: 'POST',
+    url: 'api/tasks?api_key=1',
+    contentType: 'application/json',
+    dataType: 'json',
+    data: JSON.stringify({
+      task: {
+        content: $('#new-task-content').val()
+      }
+    }),
+    success: function(response, textStatus) {
+      $('#new-task-content').val('');
+      getAndDisplayAllTasks();
+    },
+    error: function(request, textStatus, errorMessage) {
+      console.log(errorMessage);
     }
-    $.ajax(request);
-  };
-  postTask('this is some task...');
+  });
+};
 
-
-//test code
-
-export var getAndDisplayAllTasks = function (successCB, errorCB) {
-    var request = {
-      type: 'GET',
-      url: 'api/tasks?api_key=1',
-      success: successCB,
-      error: errorCB
+// Function to delete a task
+export var deleteTask = function(id) {
+  $.ajax({
+    type: 'DELETE',
+    url: 'api/tasks/' + id + '?api_key=1',
+    success: function(response, textStatus) {
+      getAndDisplayAllTasks();
+    },
+    error: function(request, textStatus, errorMessage) {
+      console.log(errorMessage);
     }
-    $.ajax(request);
-  };
-  getAndDisplayAllTasks();
-  
+  });
+};
 
+// Function to mark a task as complete
+export var markTaskComplete = function(id) {
+  $.ajax({
+    type: 'PUT',
+    url: 'api/tasks/' + id + '/mark_complete?api_key=1',
+    dataType: 'json',
+    success: function(response, textStatus) {
+      getAndDisplayAllTasks();
+    },
+    error: function(request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+  });
+};
 
-//my code
+// Function to mark a task as active
+export var markTaskActive = function(id) {
+  $.ajax({
+    type: 'PUT',
+    url: 'api/tasks/' + id + '/mark_active?api_key=1',
+    dataType: 'json',
+    success: function(response, textStatus) {
+      getAndDisplayAllTasks();
+    },
+    error: function(request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+  });
+};
 
-/*
-var getAndDisplayAllTasks = function() {
-    $.ajax({
-        type: 'GET',
-        url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=148',
-        dataType: 'json',
-        success: function(response, textStatus) {
-            $('#todo-list').empty();
-
-
-            var filteredTasks = response.tasks.filter(function(task) {
-                return true;
-            })
-            filteredTasks.forEach(function(task) {
-                $('#todo-list').prepend('<div class="row d-flex"><div class="p-2"><input type="checkbox" class="mark-complete" data-id="' + task.id + '"' + (task.completed ? 'checked' : '') + '></div><div class="p-2"><p class="col-xs-8">' + task.content + '</p></div><div class="ml-auto p-2"><button class="delete" data-id="' + task.id + '"><i class="fa fa-trash" aria-hidden="true"></i></button></div></div><hr>');
-            });
-
-
-            $("#all").click(function(event) {
-                event.preventDefault();
-                $('#todo-list').empty();
-                var filteredTasks = response.tasks.filter(function(task) {
-                    return true;
-                })
-                filteredTasks.forEach(function(task) {
-                    $('#todo-list').prepend('<div class="row d-flex"><div class="p-2"><input type="checkbox" class="mark-complete" data-id="' + task.id + '"' + (task.completed ? 'checked' : '') + '></div><div class="p-2"><p class="col-xs-8">' + task.content + '</p></div><div class="ml-auto p-2"><button class="delete" data-id="' + task.id + '"><i class="fa fa-trash" aria-hidden="true"></i></button></div></div><hr>');
-                });
-            });
-
-            $("#completed").click(function(event) {
-                event.preventDefault();
-
-                $('#todo-list').empty();
-                var filteredTasks = response.tasks.filter(function(task) {
-                    return task.completed === true;
-                })
-                filteredTasks.forEach(function(task) {
-                    $('#todo-list').prepend('<div class="row d-flex"><div class="p-2"><input type="checkbox" class="mark-complete" data-id="' + task.id + '"' + (task.completed ? 'checked' : '') + '></div><div class="p-2"><p class="col-xs-8">' + task.content + '</p></div><div class="ml-auto p-2"><button class="delete" data-id="' + task.id + '"><i class="fa fa-trash" aria-hidden="true"></i></button></div></div><hr>');
-                });
-            });
-
-            $("#active").click(function(event) {
-                event.preventDefault();
-                $('#todo-list').empty();
-                var filteredTasks = response.tasks.filter(function(task) {
-                    return task.completed === false;
-                })
-                filteredTasks.forEach(function(task) {
-                    $('#todo-list').prepend('<div class="row d-flex"><div class="p-2"><input type="checkbox" class="mark-complete" data-id="' + task.id + '"' + (task.completed ? 'checked' : '') + '></div><div class="p-2"><p class="col-xs-8">' + task.content + '</p></div><div class="ml-auto p-2"><button class="delete" data-id="' + task.id + '"><i class="fa fa-trash" aria-hidden="true"></i></button></div></div><hr>');
-                });
-            });
-
-
-        },
-        error: function(request, textStatus, errorMessage) {
-            console.log(errorMessage);
-        }
-    });
-}
-*/
-
-
-var createTask = function() {
-    $.ajax({
-        type: 'POST',
-        url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=148',
-        contentType: 'application/json',
-        dataType: 'json',
-        data: JSON.stringify({
-            task: {
-                content: $('#new-task-content').val()
-            }
-        }),
-        success: function(response, textStatus) {
-            $('#new-task-content').val('');
-            getAndDisplayAllTasks();
-        },
-        error: function(request, textStatus, errorMessage) {
-            console.log(errorMessage);
-        }
-    });
-}
-
-$('#create-task').on('submit', function(e) {
+// Event listener for creating a task
+$(document).ready(function() {
+  $('#create-task').on('submit', function(e) {
     e.preventDefault();
     createTask();
+  });
 });
 
-getAndDisplayAllTasks();
-
-
-var deleteTask = function(id) {
-    $.ajax({
-        type: 'DELETE',
-        url: 'https://fewd-todolist-api.onrender.com/tasks/' + id + '?api_key=148',
-        success: function(response, textStatus) {
-            getAndDisplayAllTasks();
-        },
-        error: function(request, textStatus, errorMessage) {
-            console.log(errorMessage);
-        }
-    });
-}
-
+// Event listener for deleting a task
 $(document).on('click', '.delete', function() {
-    deleteTask($(this).data('id'));
+  deleteTask($(this).data('id'));
 });
 
-var markTaskComplete = function(id) {
-    $.ajax({
-        type: 'PUT',
-        url: 'https://fewd-todolist-api.onrender.com/tasks/' + id + '/mark_complete?api_key=148',
-        dataType: 'json',
-        success: function(response, textStatus) {
-            getAndDisplayAllTasks();
-        },
-        error: function(request, textStatus, errorMessage) {
-            console.log(errorMessage);
-        }
-    });
-}
-
+// Event listener for marking a task as complete or active
 $(document).on('change', '.mark-complete', function() {
-    if (this.checked) {
-        markTaskComplete($(this).data('id'));
-    } else {
-        markTaskActive($(this).data('id'));
-
-    }
-
+  if (this.checked) {
+    markTaskComplete($(this).data('id'));
+  } else {
+    markTaskActive($(this).data('id'));
+  }
 });
 
-var markTaskActive = function(id) {
-    $.ajax({
-        type: 'PUT',
-        url: 'https://fewd-todolist-api.onrender.com/tasks/' + id + '/mark_active?api_key=148',
-        dataType: 'json',
-        success: function(response, textStatus) {
-            getAndDisplayAllTasks();
-        },
-        error: function(request, textStatus, errorMessage) {
-            console.log(errorMessage);
-        }
-    });
-}
+// Ensure the initial load displays all tasks
+$(document).ready(function() {
+  getAndDisplayAllTasks();
+});
